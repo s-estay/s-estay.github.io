d18a2b1b89baf176afdf19a6d1e59b66834b5e70
Sebastian Estay <sebastian.estay@icloud.com>
2025-03-12 11:31:37 +0100

    hide/show button when inside/outside visible viewport

--- assets/js/roll-back-to-top.js
+++ assets/js/roll-back-to-top.js
@@ -1,3 +1,5 @@
+// smooth scrolling to top
+
 var scrollToTopBtn = document.getElementById("scroll-to-top-button");
 var rootElement = document.documentElement;
 
@@ -9,3 +11,15 @@ function scrollToTop() {
 }
 
 scrollToTopBtn.addEventListener("click", scrollToTop);
+
+// hide/show button when inside/outside visible viewport
+
+function handleScrollButton() {
+  if (rootElement.clientHeight < rootElement.scrollHeight) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+handleScrollButton();
